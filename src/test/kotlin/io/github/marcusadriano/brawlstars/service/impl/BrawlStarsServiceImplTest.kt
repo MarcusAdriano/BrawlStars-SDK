7ca5968d40a1158e0579b26635c46c4a4deadedd
Marcus <marcusadriano.pereira@gmail.com>
2020-06-17 22:52:51 -0300

    Add junit tests sample

---
+++ src/test/kotlin/io/github/marcusadriano/brawlstars/service/impl/BrawlStarsServiceImplTest.kt
@@ -0,0 +1,15 @@
+package io.github.marcusadriano.brawlstars.service.impl
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class BrawlStarsServiceImplTest {
+
+    @Test
+    fun player() {
+    }
+
+    @Test
+    fun battleLog() {
+    }
+}
